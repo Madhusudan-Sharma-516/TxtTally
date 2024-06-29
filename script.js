5839d882d21ffece580085c9c3b54daab807ea17
Madhusudan Sharma <138996944+Madhusudan-Sharma-516@users.noreply.github.com>
2024-06-29 09:42:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let input = document.querySelector("#textbox")
+let countTxt =  document.querySelector(".count-txt")
+
+// let letterCount = input.value.length;
+// console.dir(countTxt);
+// countTxt.innerText = `Word count: 11 |Letter count: ${letterCount}`; 
+
+
+// upper case 
+document.querySelector("#hendleUpCase").addEventListener('click', () =>{
+    input.value = input.value.toUpperCase();
+}) ;
+
+// lower case 
+document.querySelector("#hendeLowCase").addEventListener('click', () =>{
+    input.value = input.value.toLowerCase();
+}) ;
+
+// capital caase function 
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
+  }
+document.querySelector("#hendleCapCase").addEventListener('click', () =>{
+    input.value = toTitleCase(input.value);
+}) ;
+
+// SentenceCase function  
+
+function toSentenceCase(str) {
+   
+    return str
+      .split(/([.!?]\s*)/)  
+      .map((sentence, index) => {
+        if (index % 2 === 0) {
+          return sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+        } else {
+          return sentence;
+        }
+      })
+      .join('');
+  }
+
+  document.querySelector("#hendleSenCase").addEventListener('click', () =>{
+    input.value = toSentenceCase(input.value);
+}) ;
+
+
+// copy function 
+document.querySelector("#hendleCopy").addEventListener('click', function() {
+    let input = document.querySelector("#textbox");
+    input.select();
+
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(input.value).then(function() {
+        alert('Text copied to clipboard');
+      }, function(err) {
+        console.error('Failed to copy: ', err);
+      });
+    } else {
+      document.execCommand('copy');
+      alert('Text copied to clipboard');
+    }
+  });
+
+
+// clear box 
+document.querySelector("#hendleClear").addEventListener('click', () =>{
+    input.value = "";
+}) ;
