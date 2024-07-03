246ffcaac706481c508beb0ae3547a7edd9f344e
Madhusudan Sharma <138996944+Madhusudan-Sharma-516@users.noreply.github.com>
2024-07-03 23:51:48 +0530

    final update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let input = document.querySelector("#textbox")
-let countTxt =  document.querySelector(".count-txt")
+let countTxt = document.querySelector(".count-txt")
 
 function countWords(str) {
   return str.trim().split(/\s+/).filter(word => word !== '').length;
@@ -14,86 +14,93 @@ updateCounts();
 input.addEventListener('input', updateCounts);
 
 // upper case 
-document.querySelector("#hendleUpCase").addEventListener('click', () =>{
-    input.value = input.value.toUpperCase();
-}) ;
+document.querySelector("#hendleUpCase").addEventListener('click', () => {
+  input.value = input.value.toUpperCase();
+});
 
 // lower case 
-document.querySelector("#hendeLowCase").addEventListener('click', () =>{
-    input.value = input.value.toLowerCase();
-}) ;
+document.querySelector("#hendeLowCase").addEventListener('click', () => {
+  input.value = input.value.toLowerCase();
+});
 
 // capital caase function 
 function toTitleCase(str) {
-    return str.replace(
-      /\w\S*/g,
-      text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
-    );
-  }
-document.querySelector("#hendleCapCase").addEventListener('click', () =>{
-    input.value = toTitleCase(input.value);
-}) ;
+  return str.replace(
+    /\w\S*/g,
+    text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+  );
+}
+document.querySelector("#hendleCapCase").addEventListener('click', () => {
+  input.value = toTitleCase(input.value);
+});
 
 // SentenceCase function  
 
 function toSentenceCase(str) {
-   
-    return str
-      .split(/([.!?]\s*)/)  
-      .map((sentence, index) => {
-        if (index % 2 === 0) {
-          return sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
-        } else {
-          return sentence;
-        }
-      })
-      .join('');
-  }
 
-  document.querySelector("#hendleSenCase").addEventListener('click', () =>{
-    input.value = toSentenceCase(input.value);
-}) ;
+  return str
+    .split(/([.!?]\s*)/)
+    .map((sentence, index) => {
+      if (index % 2 === 0) {
+        return sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+      } else {
+        return sentence;
+      }
+    })
+    .join('');
+}
+
+document.querySelector("#hendleSenCase").addEventListener('click', () => {
+  input.value = toSentenceCase(input.value);
+});
 
 
 // copy function 
-document.querySelector("#hendleCopy").addEventListener('click', function() {
-    let input = document.querySelector("#textbox");
-    input.select();
-    let 
-
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(input.value).then(function() {
-      }, function(err) {
-        console.error('Failed to copy: ', err);
-      });
-    } else {
-      document.execCommand('copy');
-      alert('Text copied to clipboard');
-    }
-  });
+document.querySelector("#hendleCopy").addEventListener('click', function () {
+  let input = document.querySelector("#textbox");
+  navigator.clipboard.writeText(input.value)
+  .then(() => {
+    alert("Text Coppied");
+  })
+});
 
 
 // clear box 
-document.querySelector("#hendleClear").addEventListener('click', () =>{
-    input.value = "";
-}) ;
+document.querySelector("#hendleClear").addEventListener('click', () => {
+  input.value = "";
+});
+
 
 
 // dark mode 
 
 let darkBtn = document.querySelector("#darkModeSwitch");
 let darkCheck = false;
+let element = document.querySelector("html");
 
 darkBtn.addEventListener("click", () => {
-  // let element = document.body;
-  let element = document.querySelector("html");
-  
-  if(darkCheck == false){
+  if (darkCheck == false) {
     element.style.mixBlendMode = "difference";
     darkCheck = true;
-  }else{
+    localStorage.setItem("dark", darkCheck);
+    darkBtn.checked = true
+  } else {
     element.style.mixBlendMode = "normal";
     darkCheck = false;
+    localStorage.setItem("dark", darkCheck);
+    darkBtn.checked = false
   }
-  
+
+});
+
+window.addEventListener('load', () => {
+  let reDarkCheck = localStorage.getItem('dark')
+  console.log(reDarkCheck);
+  if (reDarkCheck == "true") {
+    element.style.mixBlendMode = "difference";
+    darkBtn.checked = true
+  } else {
+    element.style.mixBlendMode = "normal";
+    darkBtn.checked = false
+  }
 })
